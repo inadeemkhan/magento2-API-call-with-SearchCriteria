8acf74545512a99ee130352a8473f15116c2851c
inadeemkhan <khannadeem243@gmail.com>
2022-08-24 14:24:26 +0530

    Search Criteria code update.

--- Products/Model/ProductsCollectionManagement.php
+++ Products/Model/ProductsCollectionManagement.php
@@ -3,15 +3,88 @@ declare(strict_types=1);
 
 namespace Nadeem\Products\Model;
 
+use Magento\Framework\Registry;
+use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\Eav\Model\Entity\Attribute\Exception as AttributeException;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Catalog\Api\Data\ProductSearchResultsInterfaceFactory;
+
 class ProductsCollectionManagement implements \Nadeem\Products\Api\ProductsCollectionManagementInterface
 {
+    /**
+     * @var Registry
+     */
+    protected $_coreRegistry = null;
+    /**
+     * @var \Magento\Catalog\Model\ProductFactory
+     */
+    protected $productFactory;
+    /**
+     * @var CollectionFactory $collectionFactory
+     */
+    protected $collectionFactory;
+    /**
+     * @var SearchCriteriaBuilder $searchCriteriaBuilder
+     */
+    protected $searchCriteriaBuilder;
+    /**
+     * @var ProductSearchResultsInterfaceFactory $searchResultsFactory
+     */
+    protected $searchResultsFactory;
+    
 
     /**
-     * {@inheritdoc}
+     * GetProductReviews constructor.
+     *
+     * @param Registry $registry
+     * @param CollectionFactory $collectionFactory
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param ProductSearchResultsInterfaceFactory $searchResultsFactory
      */
-    public function getProductsCollection($param)
+    public function __construct(
+        Registry $registry,
+        CollectionFactory $collectionFactory,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        ProductSearchResultsInterfaceFactory $searchResultsFactory
+    ) {
+        $this->_coreRegistry = $registry;   
+        $this->collectionFactory = $collectionFactory;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->searchResultsFactory = $searchResultsFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getProductsCollection(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
     {
-        return 'hello api GET return the $param ' . $param;
+        $searchResults = $this->searchResultsFactory->create();
+        $searchResults->setSearchCriteria($searchCriteria);
+
+        $collection = $this->collectionFactory->create();
+        $collection ->addFieldToSelect('*');
+        
+        foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
+            foreach ($filterGroup->getFilters() as $filter) {
+                $condition = $filter->getConditionType() ?: 'eq';
+                $collection->addFieldToFilter($filter->getField(), [$condition => $filter->getValue()]);
+            }
+        }
+        
+        $searchResults->setTotalCount($collection->getSize());
+        $sortOrdersData = $searchCriteria->getSortOrders();
+        if ($sortOrdersData) {
+            foreach ($sortOrdersData as $sortOrder) {
+                $collection->addOrder(
+                    $sortOrder->getField(),
+                    ($sortOrder->getDirection() == SortOrder::SORT_ASC) ? 'ASC' : 'DESC'
+                );
+            }
+        }
+        $collection->setCurPage($searchCriteria->getCurrentPage());
+        $collection->setPageSize($searchCriteria->getPageSize());
+        $searchResults->setItems($collection->getData());
+        return $searchResults;
     }
 }
-
